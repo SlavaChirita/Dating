1aec7d8a650a76fb447ec58bc3189374b075ef9d
Slava C <veaceslav.chirita@gmail.com>
2018-12-23 00:15:21 +0200

    Initial Commit

---
+++ Dating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dating
+//
+//  Created by Slava Chirita on 12/23/18.
+//  Copyright © 2018 Slava Chirita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
